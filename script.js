a70971d0cf8b9d44dedcfca9b2a70ed6a803c247
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-02-14 12:19:15 -0500

    Fixed button errors, no errors upon click

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 var letters = "abcdefghijklmnopqrztuvwxyz";
 
-function generatePasswork() {
-  var randomValue = math.floor(Math.random() * letters.length);
+function generatePassword() {
+  var randomValue = Math.floor(Math.random() * letters.length);
   console.log("Generate Password")
   console.log(letters[randomValue]);
   return "random password"
@@ -11,7 +11,7 @@ function generatePasswork() {
 
 // Write password to the #password input
 function writePassword() {
-  console.log("Write Passowrd");
+  console.log("Write Password");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
